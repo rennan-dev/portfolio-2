786195af02d2992a855896cc91da2199c16d14e5
Rennan-Dev <rennandev7@gmail.com>
2024-10-14 11:44:42 -0400

    feat: projeto inicial

---
+++ rolagem.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.section-1', {
+    rotate: {x: 0, y: 100, z: 0},
+    duration: 2000
+});
+
+sr.reveal('.section-2', {
+    rotate: {x: 0, y: 100, z: 0},
+    duration: 2000
+});
